960889df52224386ce8fa75f5bc970053036bb19
Zach Russell <zach@protechig.com>
2014-11-29 14:23:39 -0500

    add tha to header and footer

--- lib/inc/footer.php
+++ lib/inc/footer.php
@@ -5,12 +5,14 @@
  */
 ?>
 
-</body>
-
+<?php tha_footer_before(); ?>
 <footer>
+    <?php tha_footer_top(); ?>
 
-
+    <?php tha_footer_bottom(); ?>
 </footer>
+<?php tha_footer_after(); ?>
+<?php tha_body_bottom(); ?>
 
 <?php wp_footer(); ?>
 </body>
--- lib/inc/header.php
+++ lib/inc/header.php
@@ -6,14 +6,18 @@
  */
 ?>
 <!DOCTYPE html>
+<?php tha_html_before(); ?>
 <html>
 <head>
+    <?php tha_head_top(); ?>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width">
     <!--[if lt IE 9]>
     <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
+    <?php tha_head_bottom(); ?>
     <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
+<?php tha_body_top(); ?>
