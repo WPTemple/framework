8fa96dc79c03f21926a9c2f192309f7cb660017d
Zach Russell <zach@protechig.com>
2014-11-30 17:59:25 -0500

    add permalink to the_title()

--- lib/wptemple.php
+++ lib/wptemple.php
@@ -3,7 +3,7 @@
   get_header();
 
   if ( have_posts() ) : while ( have_posts() ) : the_post();
-  the_title();
+  the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' );
   the_content();
   endwhile;
   endif;
