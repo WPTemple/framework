750a85a48b8782c065000af3ccad320ee06e7915
Zach Russell <zach@protechig.com>
2014-11-30 17:53:15 -0500

    make home url a link

--- lib/templates/header.php
+++ lib/templates/header.php
@@ -4,8 +4,8 @@ add_action( 'wpt_header', 'wpt_do_header' );
 
 function wpt_do_header() {
   ?>
-  <h1><?php bloginfo( 'name' ); ?></h1>
-  <p class="dscription"><?php bloginfo( 'description' ); ?></p>
+  <h1><a href="<?php echo esc_url( home_url( '/') ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+  <p class="description"><?php bloginfo( 'description' ); ?></p>
   <?php
 
   do_action( 'wpt_header_right' );
